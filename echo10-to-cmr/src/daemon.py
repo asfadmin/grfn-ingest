118fea43521cc52caf8a20ef6dd173b31a8fe76c
David Matz <djmatz@alaska.edu>
2017-12-19 13:23:42 -0900

    Initial version of echo10-to-cmr
    This is the original version of echo10-to-cmr from the grfn-echo10-to-cmr repo.

---
+++ echo10-to-cmr/src/daemon.py
@@ -0,0 +1,62 @@
+import json
+import boto3
+from logging import getLogger
+from botocore.client import Config
+from botocore.exceptions import ClientError
+from cmr import process_task, get_session, setup
+
+
+log = getLogger()
+
+
+def get_sfn_client(connect_timeout):
+    config = Config(connect_timeout=connect_timeout, read_timeout=connect_timeout)
+    sfn_client = boto3.client('stepfunctions', config=config)
+    return sfn_client
+
+
+def get_task(sfn_client, activity):
+    task = sfn_client.get_activity_task(activityArn=activity['arn'], workerName=activity['worker_name'])
+    return task
+
+
+def send_task_response(sfn_client, token, output=None, exception=None):
+    try:
+        if not exception:
+            sfn_client.send_task_success(taskToken=token, output=json.dumps(output))
+        else:
+            sfn_client.send_task_failure(taskToken=token, error=type(exception).__name__, cause=str(exception))
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'TaskTimedOut':
+            log.exception('Failed to send task response.  Task timed out.')
+        else:
+            raise
+
+
+def daemon_loop(config, get_remaining_time_in_millis_fcn):
+    log.info('Daemon started')
+    session = get_session(config['cmr']['cached_token'])
+    sfn_client = get_sfn_client(config['sfn_connect_timeout'])
+    while True:
+        if get_remaining_time_in_millis_fcn() < config['max_task_time_in_millis']:
+            log.info('Remaining time %s less than max task time %s.  Exiting.', get_remaining_time_in_millis_fcn(), config['max_task_time_in_millis'])
+            break
+
+        task = get_task(sfn_client, config['activity'])
+        if 'taskToken' not in task:
+            log.info('No tasks found.  Exiting')
+            break
+
+        try:
+            task_input = json.loads(task['input'])
+            output = process_task(task_input, config['cmr'], session)
+            send_task_response(sfn_client, task['taskToken'], output)
+        except Exception as e:
+            log.exception('Failed to process task.')
+            send_task_response(sfn_client, task['taskToken'], exception=e)
+
+ 
+def lambda_handler(event, context):
+    arn = context.invoked_function_arn
+    config = setup(arn)
+    daemon_loop(config['daemon'], context.get_remaining_time_in_millis)
