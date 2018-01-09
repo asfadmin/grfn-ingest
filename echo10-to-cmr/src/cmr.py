93f5074b86d772a117453abd65e2531b2a553e63
idixon <irdixon@alaska.edu>
2018-01-08 15:57:08 -0900

    Update cmr.py

--- echo10-to-cmr/src/cmr.py
+++ echo10-to-cmr/src/cmr.py
@@ -35,7 +35,7 @@ def get_file_content_from_s3(bucket, key, s3):
     return contents
 
 
-def get_cached_token(config):
+def get_cached_token(config, s3):
     try:
         return get_file_content_from_s3(config['bucket'], config['key'], s3)
     except Exception as e:
@@ -48,12 +48,12 @@ def get_granule_native_id(echo10_content):
     return granule_name
 
 
-def push_echo10_granule_to_cmr(session, echo10_content, config):
+def push_echo10_granule_to_cmr(session, echo10_content, config, s3):
     response = send_request(session, config['granule_url'], echo10_content)
     if response.status_code == 401:
         lamb = boto3.client('lambda')
         lamb.invoke(FunctionName=config['cmr_token_lambda'])
-        token = get_cached_token(config['cached_token'])
+        token = get_cached_token(config['cached_token'], s3)
         session.headers.update({'Echo-Token': token})
         response = send_request(session, config['granule_url'], echo10_content)
     response.raise_for_status()
@@ -63,5 +63,5 @@ def push_echo10_granule_to_cmr(session, echo10_content, config):
 def process_task(task_input, config, session, s3):
     log.info(task_input)
     echo10_content = get_file_content_from_s3(task_input['bucket'], task_input['key'], s3)
-    response = push_echo10_granule_to_cmr(session, echo10_content, config)
+    response = push_echo10_granule_to_cmr(session, echo10_content, config, s3)
     return {'status': response.status_code, 'text': response.text}
