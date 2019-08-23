d7868b33e9dd485310dcabee3659be3398fad334
Andrew Johnston <asjohnston@alaska.edu>
2019-08-23 15:40:00 -0800

    pylint for echo10-construction

--- echo10-construction/src/main.py
+++ echo10-construction/src/main.py
@@ -1,17 +1,16 @@
-import boto3
 import os
 import copy
-import re
 import json
-from jinja2 import Template
 from datetime import datetime
 from logging import getLogger
+import boto3
+from jinja2 import Template
 from shapely.geometry import Polygon
 
 
 log = getLogger()
 log.setLevel('INFO')
-config = json.loads(os.getenv('CONFIG'))
+CONFIG = json.loads(os.getenv('CONFIG'))
 
 TEMPLATE_FILE = 'echo10.template'
 
@@ -140,16 +139,16 @@ def create_granule_echo10_in_s3(inputs, config):
     echo10_s3_objects = []
     log.info('Creating echo10 file for %s', inputs['Product']['Key'])
     granule_data = get_granule_data(inputs, config['granule_data'])
-    echo10_content = render_granule_data_as_echo10(granule_data) 
+    echo10_content = render_granule_data_as_echo10(granule_data)
     echo10_s3_object = {
         'bucket': config['output_bucket'],
         'key': granule_data['granule_ur'] + '.echo10',
     }
-    echo10_s3_objects.append(echo10_s3_object)        
+    echo10_s3_objects.append(echo10_s3_object)
     upload_content_to_s3(echo10_s3_object, echo10_content)
-    
+
     del granule_data['size_mb_data_granule']
-    del granule_data['additional_attributes']['BYTES']  
+    del granule_data['additional_attributes']['BYTES']
 
     for product in config['derived_products']:
         virtual_granule_data = copy.deepcopy(granule_data)
@@ -158,13 +157,13 @@ def create_granule_echo10_in_s3(inputs, config):
         virtual_granule_data['granule_ur'] = granule_data['granule_ur'] + '-' + product['label']
         virtual_granule_data['additional_attributes']['PROCESSING_TYPE'] = product['processing_type']
         virtual_granule_data['additional_attributes']['PROCESSING_TYPE_DISPLAY'] = product['processing_type_display']
-        virtual_granule_data['online_access_url'] = '{0}?product={1}&amp;layer={2}'.format(config['api_url'],inputs['Product']['Key'],product['layer'])
+        virtual_granule_data['online_access_url'] = '{0}?product={1}&amp;layer={2}'.format(config['api_url'], inputs['Product']['Key'], product['layer'])
         echo10_content = render_granule_data_as_echo10(virtual_granule_data)
         echo10_s3_object = {
             'bucket': config['output_bucket'],
             'key': virtual_granule_data['granule_ur'] + '.echo10',
         }
-        echo10_s3_objects.append(echo10_s3_object)        
+        echo10_s3_objects.append(echo10_s3_object)
 
         upload_content_to_s3(echo10_s3_object, echo10_content)
 
@@ -172,5 +171,5 @@ def create_granule_echo10_in_s3(inputs, config):
 
 
 def lambda_handler(event, context):
-    output = create_granule_echo10_in_s3(event, config)
+    output = create_granule_echo10_in_s3(event, CONFIG)
     return output
