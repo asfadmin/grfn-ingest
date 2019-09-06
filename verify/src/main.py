d86fedd3489614769b576f9db167779656faf010
Andrew Johnston <asjohnston@alaska.edu>
2019-09-06 22:08:03 +0000

    more cleanup of verify exception messages

--- verify/src/main.py
+++ verify/src/main.py
@@ -41,7 +41,7 @@ def validate_s3_object(obj):
         s3.Object(obj['Bucket'], obj['Key']).load()
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] in ['403', '404']:
-            raise MISSING_FILE(str(obj) + ' ' + e.message)
+            raise MISSING_FILE(str(obj) + ' ' + str(e))
         raise
 
 
@@ -58,10 +58,15 @@ def validate_message(message):
 def validate_metadata(obj):
     metadata = get_file_content_from_s3(obj['Bucket'], obj['Key'])
     metadata_schema = get_json_from_file('metadata_schema.json')
+
     try:
         metadata = json.loads(metadata)
+    except json.decoder.JSONDecodeError as e:
+        raise INVALID_METADATA(str(e))
+
+    try:
         jsonschema.validate(metadata, metadata_schema)
-    except (ValueError, jsonschema.exceptions.ValidationError) as e:
+    except jsonschema.exceptions.ValidationError as e:
         raise INVALID_METADATA(e.message)
 
 
@@ -79,7 +84,7 @@ def validate_topic(topic):
         sns.publish(TopicArn=topic['Arn'], Message='invalidMessage', MessageStructure='json')
     except botocore.exceptions.ClientError as e:
         if not json_error(e.response['Error']):
-            raise INVALID_TOPIC(e.message)
+            raise INVALID_TOPIC(str(e))
 
 
 def verify(message):
