5c7c9270845d5ca20713a072c7cb029f06cc1e36
Andrew Johnston <asjohnston@alaska.edu>
2022-12-16 17:32:33 -0900

    use a single json-encoded launchpad secret

--- cmr-token/src/main.py
+++ cmr-token/src/main.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import os
 from logging import getLogger
 
@@ -10,14 +12,12 @@ s3 = boto3.client('s3')
 secrets_manager = boto3.client('secretsmanager')
 
 
-def get_secret_value(secret_arn: str):
+def get_secret_value(secret_arn: str) -> dict:
     response = secrets_manager.get_secret_value(SecretId=secret_arn)
-    if 'SecretBinary' in response:
-        return response['SecretBinary']
-    return response['SecretString']
+    return json.loads(response['SecretString'])
 
 
-def get_new_token(certificate: bytes, passphrase: str):
+def get_new_token(certificate: bytes, passphrase: str) -> str:
     response = requests_pkcs12.get(
         'https://api.launchpad.nasa.gov/icam/api/sm/v1/gettoken',
         pkcs12_data=certificate,
@@ -29,7 +29,9 @@ def get_new_token(certificate: bytes, passphrase: str):
 
 
 def lambda_handler(event, context):
-    certificate = get_secret_value(os.environ['CERTIFICATE_SECRET_ARN'])
-    passphrase = get_secret_value(os.environ['PASSPHRASE_SECRET_ARN'])
+    secret = get_secret_value(os.environ['CERTIFICATE_SECRET_ARN'])
+    certificate = base64.b64decode(secret['certificate'])
+    passphrase = secret['passphrase']
+
     token = get_new_token(certificate, passphrase)
     s3.put_object(Bucket=os.environ['BUCKET'], Key=os.environ['KEY'], Body=token)
